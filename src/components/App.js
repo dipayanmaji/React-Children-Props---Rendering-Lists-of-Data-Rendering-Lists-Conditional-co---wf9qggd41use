adda3294ffa3bd5fb5d7a06ca0f4305c6d40442b
Dipayan Maji <dipayanmaji1112@gmail.com>
2023-02-17 16:41:01 +0530

    trying 2

--- src/components/App.js
+++ src/components/App.js
@@ -4,15 +4,11 @@ import ChildComponent from './ChildComponent';
 const App = () => {
   //code here
   let [square, setSquare] = useState(0);
-  const findSquare = (event)=>{
-    let value = event.target.value;
-    // console.log(value);
-    setSquare(value*value);
-  }
+
   return (
     <div id="main">
-      <ChildComponent /> 
-        <input id='input' onChange={findSquare} />
+      <ChildComponent setSquare={setSquare} /> 
+        {/* <input id='input' onChange={findSquare} /> */}
     
       <p id='output'>{square}</p>
     </div>
--- src/components/ChildComponent.js
+++ src/components/ChildComponent.js
@@ -1,10 +1,17 @@
 import React from 'react'
 
-const ChildComponent = () => {
-//code here
+const ChildComponent = (props) => {
+  //code here
+  let setSquare = props.setSquare;
+  const findSquare = (event)=>{
+  let value = event.target.value;
+  // console.log(value);
+  setSquare(value*value);
+}
   return (
     <div id="child">
       Find The Square <br/>
+      <input id='input' onChange={findSquare} />
      
     </div>
   )
